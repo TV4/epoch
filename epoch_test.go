bebb5bc68af363089dd8dcf236b0ca5d43753af6
Infrastruktur-mobben <infrastruktur@tv4.se>
2017-08-15 16:32:30 +0200

    Simplify test cases
    There is no need to wrap everything in a struct, a plain value by itself
    is also valid JSON.
    
    https://tools.ietf.org/html/rfc7159#section-13

--- epoch_test.go
+++ epoch_test.go
@@ -6,22 +6,13 @@ import (
 	"time"
 )
 
-type tstruct struct {
-	E Time `json:"ts"`
-}
-
-var (
-	ttest = time.Unix(1, 1003000000)
-)
-
 func TestMarshal(t *testing.T) {
-	ts := tstruct{Time(ttest)}
-
-	b, err := json.Marshal(ts)
+	e := Time(time.Unix(1, 1003000000))
+	b, err := json.Marshal(e)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if got := string(b); got != `{"ts":2003}` {
+	if got := string(b); got != `2003` {
 		t.Errorf("unexpected json: %s\n", got)
 	}
 }
@@ -31,27 +22,27 @@ func TestUnmarshal(t *testing.T) {
 		s string
 		i int64
 	}{
-		{`{"ts":1}`, 1000000000},
-		{`{"ts":1.03}`, 1030000000},
-		{`{"ts":0.00000003}`, 30},
-		{`{"ts":0.000000003}`, 3},
-		{`{"ts":0.0000000003}`, 0},
-		{`{"ts":"1000000001"}`, 1000000001000000000},
-		{`{"ts":1000000001.030}`, 1000000001030000000},
-		{`{"ts":1000000001.300}`, 1000000001300000000},
-		{`{"ts":1000000001.000000001}`, 1000000001000000001},
-		{`{"ts":1000000001.0300000}`, 1000000001030000000},
-		{`{"ts":1000000001101}`, 1000000001101000000},
-		{`{"ts":1000000001101.001}`, 1000000001101001000},
-		{`{"ts":1000000001000001.1}`, 1000000001000001100},
-		{`{"ts":1000000001000000001}`, 1000000001000000001},
+		{`1`, 1000000000},
+		{`1.03`, 1030000000},
+		{`0.00000003`, 30},
+		{`0.000000003`, 3},
+		{`0.0000000003`, 0},
+		{`"1000000001"`, 1000000001000000000},
+		{`1000000001.030`, 1000000001030000000},
+		{`1000000001.300`, 1000000001300000000},
+		{`1000000001.000000001`, 1000000001000000001},
+		{`1000000001.0300000`, 1000000001030000000},
+		{`1000000001101`, 1000000001101000000},
+		{`1000000001101.001`, 1000000001101001000},
+		{`1000000001000001.1`, 1000000001000001100},
+		{`1000000001000000001`, 1000000001000000001},
 	} {
-		var tt tstruct
-		err := json.Unmarshal([]byte(tst.s), &tt)
+		var e Time
+		err := json.Unmarshal([]byte(tst.s), &e)
 		if err != nil {
 			t.Fatal(err, tst.s)
 		}
-		rt := time.Time(tt.E)
+		rt := time.Time(e)
 		if rt.UnixNano() != tst.i {
 			t.Error("unexpected time:", rt.UnixNano(), tst.i)
 		}
@@ -59,18 +50,16 @@ func TestUnmarshal(t *testing.T) {
 }
 
 func TestBadInput(t *testing.T) {
-	for _, tst := range []struct {
-		s string
-	}{
-		{`{"ts":"bad"}`},
-		{`{"ts":"1.2.3"}`},
-		{`{"ts":"1.a"}`},
-		{`{"ts":"."}`},
+	for _, s := range []string{
+		`"bad"`,
+		`"1.2.3"`,
+		`"1.a"`,
+		`"."`,
 	} {
-		var tt tstruct
-		err := json.Unmarshal([]byte(tst.s), &tt)
+		var e Time
+		err := json.Unmarshal([]byte(s), &e)
 		if err == nil {
-			t.Fatal("expected error but got nil", tst.s)
+			t.Fatal("expected error but got nil", s)
 		}
 	}
 }
